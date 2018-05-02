78218fc10b25b60ce231300e47caceaf28175156
Isht Dwivedi <isht.dwivedi@gmail.com>
2018-05-02 15:35:11 -0400

    Update eval_r5.py

--- exp-src/eval_r5.py
+++ exp-src/eval_r5.py
@@ -78,7 +78,7 @@ counter = 0
 model.cuda()
 snapPrefix= args['--snapPrefix']
 for iter in range(1,21):
-    saved_state_dict = torch.load('snapshots/'+snapPrefix+str(iter)+'000.pth')
+    saved_state_dict = torch.load('data/snapshots/'+snapPrefix+str(iter)+'000.pth')
     if counter==0:
 	print snapPrefix
     counter+=1
